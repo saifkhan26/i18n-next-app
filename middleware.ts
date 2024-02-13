eb43ddf462ebb6c121e36c9192ee87130fbcea35
saifkhan26 <saifuddinkhan1226@gmail.com>
2024-02-13 11:51:20 +0500

    first commit

--- middleware.ts
+++ middleware.ts
@@ -20,23 +20,14 @@ function getLocale(request: NextRequest): string | undefined {
   );
 
   const locale = matchLocale(languages, locales, i18n.defaultLocale);
+  console.log(languages, locales, i18n.defaultLocale)
+  console.log(negotiatorHeaders)
 
   return locale;
 }
 
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
-
-  // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
-  // // If you have one
-  // if (
-  //   [
-  //     '/manifest.json',
-  //     '/favicon.ico',
-  //     // Your other files in `public`
-  //   ].includes(pathname)
-  // )
-  //   return
 
   // Check if there is any supported locale in the pathname
   const pathnameIsMissingLocale = i18n.locales.every(
